d0951f0551b7574bd386a585a16b66a3de502059
Yohanan Baruchel <ybaruchel@gmail.com>
2019-01-28 20:56:11 +0200

    Add ramilevy

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-from Spiders import Shufersal
+from scrappers import shufersal
+from scrappers import ramilevy
 from scrapy.crawler import CrawlerProcess
 import logging
 
 
 class Main:
     def __init__(self):
-        self.init_shufersal(self)
+        self.init_shufersal()
+        self.init_rami_levy()
 
     @staticmethod
-    def init_shufersal(self):
+    def init_shufersal():
         logging.getLogger('scrapy').propagate = False
         print("Crawling Shufersal...")
+
         process = CrawlerProcess({
             'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
         })
-        process.crawl(Shufersal.Shufersal())
+        process.crawl(shufersal.Shufersal())
         process.start()
+
+    @staticmethod
+    def init_rami_levy():
+        print("Crawling Rami Levy...")
+
+        ramilevy.RamiLevy().scrape()
+
 
 
 if __name__ == '__main__':
